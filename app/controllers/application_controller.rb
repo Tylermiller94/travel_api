7bdb09238dbf55754e6d34b24d4ec8610847276f
Kristen Kulha and Tyler Miller <kristen+tyler@>
2017-12-11 08:59:12 -0800

    Add exception for active record record not found error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include Response
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    json_respone({ messageL exception.nessage }, :not_found)
+  end
 end
